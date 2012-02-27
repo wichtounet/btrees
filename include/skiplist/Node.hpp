7dd84b1f67a22dd876a664f21a7a3e3556285aae
Baptiste Wicht <baptistewicht@gmail.com>
2012-02-28 00:51:04 +0100

    Cleanup...

--- include/skiplist/Node.hpp
+++ include/skiplist/Node.hpp
@@ -8,7 +8,6 @@ class Node {
         const T value;
         const int key;
         Node<T>* next[MAX_LEVEL + 1];
-        //Node<T>** next;//TODO Check if possible to declare directly static array
         const int length;
         int topLevel;
 
@@ -27,12 +26,9 @@ class Node {
 
 template<typename T>
 Node<T>::Node(int k) : value(k), key(k), length(MAX_LEVEL + 1), topLevel(MAX_LEVEL) {
-
     for(int i = 0; i < MAX_LEVEL + 1; ++i){
         next[i] = nullptr;
     }
-
-    //next = (Node<T>**) calloc(length, sizeof(Node<T>*));
 }
 
 template<typename T>
@@ -40,13 +36,11 @@ Node<T>::Node(T x, int height) : value(x), key(hash(x)), length(height + 1), top
     for(int i = 0; i < MAX_LEVEL + 1; ++i){
         next[i] = nullptr;
     }
-    //next = (Node<T>**) calloc(length, sizeof(Node<T>*));
 }
 
 template<typename T>
 Node<T>::~Node(){
     std::cout << "caca" << std::endl;
-    //free(next);
 }
 
 }
