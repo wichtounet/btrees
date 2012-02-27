5937c2177e99b2cb6a567e797e85468ad9ae4b22
Baptiste Wicht <baptistewicht@gmail.com>
2012-02-27 11:42:32 +0100

    Free the memory

--- include/skiplist/Node.hpp
+++ include/skiplist/Node.hpp
@@ -9,10 +9,10 @@ class Node {
     public:
         const T value;
         const int key;
-        Node<T>* next;//TODO Check if possible to declare directly static array
+        Node<T>** next;//TODO Check if possible to declare directly static array
 
     private:
-        int toplevel;
+        int topLevel;
 
     public:
         //constructor for sentinel nodes
@@ -20,16 +20,32 @@ class Node {
 
         //constructor for ordinary nodes
         Node(T x, int height);
+
+        ~Node();
 };
 
 template<typename T>
-Node<T>::Node(int k) : value(k), key(k) {
-    //TODO
+Node<T>::Node(int k) : value(k), key(k), topLevel(MAX_LEVEL) {
+
+    next = (Node<T>**) malloc(sizeof(Node<T>*) * (MAX_LEVEL + 1));
+
+    for(int i = 0; i < MAX_LEVEL + 1; ++i){
+        next[i] = nullptr;
+    }
 }
 
 template<typename T>
-Node<T>::Node(T x, int height) : value(x), key(hash(x)) {
-    //TODO
+Node<T>::Node(T x, int height) : value(x), key(hash(x)), topLevel(height) {
+    next = (Node<T>**) malloc(sizeof(Node<T>*) * (height + 1));
+
+    for(int i = 0; i < MAX_LEVEL + 1; ++i){
+        next[i] = nullptr;
+    }
+}
+
+template<typename T>
+Node<T>::~Node(){
+    free(next);
 }
 
 }
