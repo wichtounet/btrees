291d67949e5c51e263afa10231bb6a0fc4b7d4cd
Baptiste Wicht <baptistewicht@gmail.com>
2012-03-02 11:09:27 +0100

    Implement the contains operation

--- include/avltree/AVLTree.hpp
+++ include/avltree/AVLTree.hpp
@@ -22,15 +22,37 @@ struct Node {
     volatile Node* right;
     
     const int key;
+
+    volatile Node* child(int direction){
+        if(direction > 0){
+            return right;
+        } else if(direction < 0){
+            return left;
+        }
+
+        assert(false);
+    }
+};
+
+enum Result {
+    FOUND,
+    NOT_FOUND,
+    RETRY
 };
 
 template<typename T>
 class AVLTree {
     public:
         AVLTree();
+        
+        bool contains(T value);
 
     private:
         Node* rootHolder;
+
+        Result attemptGet(int key, Node* node, int dir, long nodeV);
+
+        void waitUntilNotChanging(Node* node);
 };
 
 template<typename T>
@@ -38,6 +60,48 @@ AVLTree<T>::AVLTree(){
     rootHolder = new Node();
 
     rootHolder->right = new Node();
+}
+        
+template<typename T>        
+bool AVLTree<T>::contains(T value){
+    return attemptGet(hash(value), rootHolder, 1, 0) == FOUND;
+}
+
+template<typename T>
+Result AVLTree<T>::attemptGet(int key, Node* node, int dir, long nodeV){
+    while(true){
+        Node* child = node->child(dir);
+
+        if(((node->version ^ nodeV) & IgnoreGrow) != 0){
+            return RETRY;
+        }
+
+        if(!child){
+            return NOT_FOUND;
+        }
+
+        int nextD = key - child->key; //Compare to the other key
+
+        if(nextD == 0){
+            return FOUND;
+        }
+
+        long chV = child->version;
+
+        if((chV & Shrinking) != 0){
+            waitUntilNotChanging(child);
+        } else if( chV != Unlinked && child == node->child(dir)){
+            if(((node->version^nodeV) & IgnoreGrow) != 0){
+                return RETRY;
+            }
+
+            Result result = attemptGet(key, child, nextD, chV);
+            if(result != RETRY){
+                return result;
+            }
+
+        }
+    }
 }
 
 } //end of avltree
