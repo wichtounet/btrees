3c609fc656dcaf386e2b7194886f0ff6e993c861
Baptiste Wicht <baptistewicht@gmail.com>
2012-03-25 16:13:41 +0200

    Improve the CMake configuration

--- src/main.cpp
+++ src/main.cpp
@@ -253,10 +253,10 @@ void bench(unsigned int range, unsigned int add, unsigned int remove){
     std::cout << "Bench with " << OPERATIONS << " operations/thread, range = " << range << ", " << add << "% add, " << remove << "% remove, " << (100 - add - remove) << "% contains" << std::endl;
 
     //BENCH(skiplist::SkipList, "SkipList", range, add, remove);
-    //BENCH(nbbst::NBBST, "Non-Blocking Binary Search Tree", range, add, remove);
+    BENCH(nbbst::NBBST, "Non-Blocking Binary Search Tree", range, add, remove);
     //BENCH(avltree::AVLTree, "Optimistic AVL Tree", range, add, remove)
     //BENCH(lfmst::MultiwaySearchTree, "Lock-Free Multiway Search Tree", range, add, remove);
-    BENCH(cbtree::CBTree, "Counter Based Tree", range, add, remove);
+    //BENCH(cbtree::CBTree, "Counter Based Tree", range, add, remove);
 }
 
 void bench(unsigned int range){
