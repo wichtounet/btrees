d6cea26f8c1ac75c35e31bb3f6a6db8050a977de
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-04-22 17:20:54 +0200

    Rewrite memory usage benchmark to use malloc hooks instead of readproc

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,3 +1,38 @@
+#include <malloc.h>
+
+static void my_init_hook(void);
+static void* my_malloc_hook(size_t, const void*);
+
+void (*__malloc_initialize_hook) (void) = my_init_hook;
+
+static void* (*old_malloc_hook)(size_t, const void*);
+
+static void my_init_hook(void){
+    old_malloc_hook = __malloc_hook;
+    __malloc_hook = my_malloc_hook;
+}
+
+static unsigned long allocated = 0;
+
+static void * my_malloc_hook (size_t size, const void*){
+    void *result;
+    /* Restore all old hooks */
+    __malloc_hook = old_malloc_hook;
+    /* Call recursively */
+    result = malloc (size);
+    /* Save underlying hooks */
+    old_malloc_hook = __malloc_hook;
+    //Inc the allocated size
+    allocated += size;
+    /* printf might call malloc, so protect it too. */
+    if(allocated % 1000000000 == 0){
+        printf ("allocated=%lu\n", allocated);
+    }
+    /* Restore our own hooks */
+    __malloc_hook = my_malloc_hook;
+    return result;
+}
+
 #include <string>
 #include <vector>
 #include <iostream>
@@ -5,11 +40,8 @@
 #include <algorithm>
 #include <set>
 
-//For reading memory usage
-#include <stdio.h>
-#include <proc/readproc.h>
-
 #include "memory.hpp"
+#include "Results.hpp"
 
 //Include all the trees implementations
 #include "skiplist/SkipList.hpp"
@@ -17,6 +49,10 @@
 #include "avltree/AVLTree.hpp"
 #include "lfmst/MultiwaySearchTree.hpp"
 #include "cbtree/CBTree.hpp"
+
+unsigned int memory_kb(double size){
+    return size / 1024.0;
+}
 
 std::string memory(double size){
     std::stringstream stream;
@@ -35,41 +71,38 @@ std::string memory(double size){
 }
 
 template<typename Tree>
-void memory(const std::string& name, int size){
+void memory(const std::string& name, unsigned int size, Results& results){
     //Use random insertion in order to support non-balanced version
     std::vector<int> elements;
-    for(int i = 0; i < size; ++i){
+    for(unsigned int i = 0; i < size; ++i){
         elements.push_back(i);
     }
 
     random_shuffle(elements.begin(), elements.end());
 
-    struct proc_t usage1;
-    struct proc_t usage2;
-
-    //Lookup for usage
-    look_up_our_self(&usage1);
+    //For now on, count all the allocations
+    allocated = 0;
 
     Tree tree;
 
     //Fill the tree
-    for(int i = 0; i < size; ++i){
+    for(unsigned int i = 0; i < size; ++i){
         tree.add(elements[i]);
     }
     
-    //Lookup for usage
-    look_up_our_self(&usage2);
+    unsigned long usage = allocated;
     
-    std::cout << name << "-" << size << " is using " << memory(usage2.vsize - usage1.vsize)  << std::endl;
+    std::cout << name << "-" << size << " is using " << (usage / 1024) << " KB" << std::endl;
+    results.add_result(name, (usage / 1024.0));
     
     //Empty the tree
-    for(int i = 0; i < size; ++i){
+    for(unsigned int i = 0; i < size; ++i){
         tree.remove(i);
     }
 }
 
 template<typename Tree>
-void memory_high(const std::string& name, unsigned int size){
+void memory_high(const std::string& name, unsigned int size, Results& results){
     std::mt19937_64 engine(time(0));
 
     std::uniform_int_distribution<int> valueDistribution(0, std::numeric_limits<int>::max());
@@ -80,11 +113,8 @@ void memory_high(const std::string& name, unsigned int size){
         elements.insert(valueGenerator());
     }
 
-    struct proc_t usage1;
-    struct proc_t usage2;
-
-    //Lookup for usage
-    look_up_our_self(&usage1);
+    //For now on, count all the allocations
+    allocated = 0;
 
     Tree tree;
 
@@ -92,11 +122,11 @@ void memory_high(const std::string& name, unsigned int size){
     for(auto i : elements){
         tree.add(i);
     }
+
+    unsigned long usage = allocated;
     
-    //Lookup for usage
-    look_up_our_self(&usage2);
-    
-    std::cout << name << "-" << size << " is using " << memory(usage2.vsize - usage1.vsize)  << std::endl;
+    std::cout << name << "-" << size << " is using " << (usage / 1024) << " KB" << std::endl;
+    results.add_result(name, (usage / 1024.0));
     
     //Empty the tree
     for(auto i : elements){
@@ -107,16 +137,19 @@ void memory_high(const std::string& name, unsigned int size){
 void test_memory_consumption(){
     std::cout << "Test the memory consumption of each version" << std::endl;
 
-    std::vector<int> sizes = {1000, 10000, 100000, 1000000, 10000000};
+    std::vector<unsigned int> sizes = {1000, 10000, 100000, 1000000, 10000000};
+        
+    Results results;
+    results.start("memory");
 
     for(auto size : sizes){
-        //memory_high<skiplist::SkipList<int, 32>>("SkipList", size);
-        //memory_high<nbbst::NBBST<int, 32>>("NBBST", size);
+        memory<skiplist::SkipList<int, 32>>("skiplist", size, results);
+        memory<nbbst::NBBST<int, 32>>("nbbst", size, results);
+        //LFMST
+        memory<avltree::AVLTree<int, 32>>("avltree", size, results);
+        memory<cbtree::CBTree<int, 32>>("cbtree", size, results);
     }
-    
-    for(auto size : sizes){
-        //memory<skiplist::SkipList<int, 32>>("SkipList", size);
-        //memory<nbbst::NBBST<int, 32>>("NBBST", size);
-    }
+
+    results.finish();
 }
 
